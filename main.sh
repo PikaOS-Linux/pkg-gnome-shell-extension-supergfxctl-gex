8475997e35fc8f72c8be4ca9e92dcb12874f8cdf
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-05-12 17:04:32 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -12,7 +12,7 @@ mkdir -p ./gnome-shell-extension-supergfxctl-gex
 cp -rvf ./debian ./gnome-shell-extension-supergfxctl-gex/
 cd ./gnome-shell-extension-supergfxctl-gex
 mkdir -p ./usr/share/gnome-shell/extensions/supergfxctl-gex@asus-linux.org
-unzip ../supergfxctl-gexasus-linux.org.v30.shell-extension.zip -d /usr/share/gnome-shell/extensions/supergfxctl-gex@asus-linux.org/
+unzip ../supergfxctl-gexasus-linux.org.v30.shell-extension.zip -d ./usr/share/gnome-shell/extensions/supergfxctl-gex@asus-linux.org/
 
 # Get build deps
 ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
