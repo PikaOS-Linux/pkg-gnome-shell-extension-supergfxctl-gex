937bcee67f3c07ec25ae0eee014bd1b262b50728
ferrreo <harderthanfire@gmail.com>
2023-06-16 19:01:31 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,5 @@
 DEBIAN_FRONTEND=noninteractive
 
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
 apt install -y rpm
 wget https://extensions.gnome.org/extension-data/supergfxctl-gexasus-linux.org.v30.shell-extension.zip
@@ -15,8 +10,6 @@ mkdir -p ./usr/share/gnome-shell/extensions/supergfxctl-gex@asus-linux.org
 unzip ../supergfxctl-gexasus-linux.org.v30.shell-extension.zip -d ./usr/share/gnome-shell/extensions/supergfxctl-gex@asus-linux.org/
 
 # Get build deps
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get build-dep ./ -y
 
 # Build package
